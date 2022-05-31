d5dcbd23f20bea51dba4ab313f0d1f599a6e4742
sosoKim9966 <sosokim9966@gmail.com>
2022-05-31 16:43:35 +0900

    2022/05/31-new3

--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
@@ -3,10 +3,12 @@ package com.jojoldu.book.freelecspringboot2webservice.web;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
